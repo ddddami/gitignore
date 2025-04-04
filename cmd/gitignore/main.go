42b9aea2ca2469e70c460b24b078d4ce9a2c8219
Damilola O. <82976159+ddddami@users.noreply.github.com>
2025-04-04 11:25:20 +0100

    List available templates

--- cmd/gitignore/main.go
+++ cmd/gitignore/main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"embed"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+//go:embed templates
+var templateFS embed.FS
 
 func main() {
-	fmt.Println("ini")
+	list := flag.Bool("list", false, "List available templates")
+	flag.Parse()
+
+	if *list {
+		templates, err := listTemplates()
+		if err != nil {
+			fmt.Println("An error occured lisiting available templates")
+			os.Exit(1)
+		}
+		fmt.Println("Available templates:")
+		for _, t := range templates {
+			fmt.Println(" -", t)
+		}
+		return
+	}
+
+	if len(flag.Args()) == 0 {
+		fmt.Println("Please specify a template, e.g 'node', 'python'. Run `list` to list all available templates")
+		fmt.Println("Available templates...")
+		listTemplates()
+		return
+	}
+}
+
+func listTemplates() ([]string, error) {
+	entries, err := templateFS.ReadDir("templates")
+	if err != nil {
+		return nil, err
+	}
+	var templates []string
+	for _, entry := range entries {
+		if !entry.IsDir() {
+
+			name := entry.Name()
+			templates = append(templates, strings.TrimSuffix(name, filepath.Ext(name)))
+		}
+	}
+	return templates, nil
 }
